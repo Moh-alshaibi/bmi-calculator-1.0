903007c373c58e8c3a5c2d1838f504aef9d56eae
Moh-alshaibi <kib09005@gmail.com>
2023-02-18 01:36:24 +0100

    finished logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,23 +1,30 @@
+'use strict';
+
 function calculateBMI() {
-    var weight = document.getElementById("weight").value;
-    var height = document.getElementById("height").value / 100; // convert cm to m
-    var bmi = weight / (height * height);
-  
-    var resultElement = document.getElementById("result");
-    resultElement.innerHTML = "Your BMI is " + bmi.toFixed(1);
-  
-    if (bmi < 18.5) {
-      resultElement.style.color = "#FFC107"; // yellow
-      resultElement.innerHTML += " - Underweight";
-    } else if (bmi < 25) {
-      resultElement.style.color = "#4CAF50"; // green
-      resultElement.innerHTML += " - Normal weight";
-    } else if (bmi < 30) {
-      resultElement.style.color = "#FF9800"; // orange
-      resultElement.innerHTML += " - Overweight";
-    } else {
-      resultElement.style.color = "#F44336"; // red
-      resultElement.innerHTML += " - Obese";
-    }
+  // Get user inputs
+  const age = document.getElementById("age").value;
+  const weight = document.getElementById("weight").value;
+  const height = document.getElementById("height").value;
+  const sex = document.querySelector('input[name="sex"]:checked').value;
+
+  // Convert height to meters
+  const heightMeters = height / 100;
+
+  // Calculate BMI
+  const bmi = weight / (heightMeters * heightMeters);
+
+  // Display result
+  let message  = `Your BMI is ${bmi.toFixed(2)}.`
+
+  if (bmi < 18.5) {
+    message += "You are underweight.";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    message += "Your weight is normal.";
+  } else if (bmi >= 25 && bmi < 30) {
+    message += "You are overweight.";
+  } else {
+    message += "You are obese.";
   }
-  
+
+  document.getElementById("result").innerHTML = message;
+}
