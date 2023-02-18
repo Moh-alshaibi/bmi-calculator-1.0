79a46ddb56f9509e0e9abfcf1ffdba467ad836d6
Moh-alshaibi <kib09005@gmail.com>
2023-02-18 01:38:32 +0100

    cleaned up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,16 @@
 'use strict';
 
 function calculateBMI() {
-  // Get user inputs
+  
   const age = document.getElementById("age").value;
   const weight = document.getElementById("weight").value;
   const height = document.getElementById("height").value;
   const sex = document.querySelector('input[name="sex"]:checked').value;
 
-  // Convert height to meters
   const heightMeters = height / 100;
 
-  // Calculate BMI
   const bmi = weight / (heightMeters * heightMeters);
 
-  // Display result
   let message  = `Your BMI is ${bmi.toFixed(2)}.`
 
   if (bmi < 18.5) {
